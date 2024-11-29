5e2be871b7fc5c46a164d972f293e809ca08d4e1
eduardo.assuncao <eduardo.assuncao@tce.ma.gov.br>
2024-11-29 12:00:08 -0300

    deploy

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+    // PostgreSQL Driver
+    runtimeOnly("org.postgresql:postgresql:42.6.0")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
